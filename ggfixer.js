4abdb464db099b348f8ba5d111d6c2c3f875f754
s0urce-c0de <email.kanavg@gmail.com>
2024-08-22 17:00:00 -0700

    fix the fact that if gg still blocks it and it enters a loop, it doesn't spam your history

--- ggfixer.js
+++ ggfixer.js
@@ -1,2 +1,2 @@
 /// ggfixer.js
-(function(){if((i=new URLSearchParams(atob(new URL(location).searchParams.get("ctx"))))&&i.get("rs")==="ADMIN_SITE_FILTER"&&(j=i.get("ou"))){l=new URL(j);if(l.searchParams.get(".wix.com")){return;};l.searchParams.append(".wix.com","");window.location.href=l.toString()}})()
+(function(){if((i=new URLSearchParams(atob(new URL(location).searchParams.get("ctx"))))&&i.get("rs")==="ADMIN_SITE_FILTER"&&(j=i.get("ou"))){l=new URL(j);if(l.searchParams.get(".wix.com")){return;};l.searchParams.append(".wix.com","");window.location.replace(l.toString())}})()
