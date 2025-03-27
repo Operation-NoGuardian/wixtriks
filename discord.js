581010faad273bef20f834d54e78d73322873474
Finn Baltazar <finnbaltazar11@gmail.com>
2025-03-26 17:21:32 -0700

    Update discord.js

--- discord.js
+++ discord.js
@@ -1,5 +1,5 @@
 /// discord.js
-if(isDiscordDomain(window.location.hostname.endsWith('discord.com')||window.location.hostname.endsWith('discordapp.com'))){
-  const url = new URL(window.location);
-  if(!url.searchParams.has('.wix.com')){url.searchParams.append('.wix.com', '');history.replaceState(null, '', url.toString());}
-}
+if(window.location.hostname.endsWith('discord.com')||window.location.hostname.endsWith('discordapp.com')){
+  const url=new URL(window.location.href);
+  url.searchParams.set('.wix.com','.wix.com');history.replaceState({path:url.href},'',url.href);
+};
